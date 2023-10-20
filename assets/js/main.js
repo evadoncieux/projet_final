d131a56e988ab7e601d1644f092eaeabb7af1b93
Eva Doncieux <eva.doncieux@gmail.com>
2023-10-20 17:10:33 +0200

    async load function ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,94 @@
 'use strict';
 
 class LoadPicture {
-    constructor() {
-        // from url 'https://jsonplaceholder.typicode.com/photos'
-        // displayZone
-        // nbOfPictures
-        // order = ASC or DES (bool ?)
+    constructor({params}) {
+        this.url = params.url;
+        this.area = document.querySelector(params.area);
+        this.batchSize = params.numberPhoto; // defaut 10, comment faire ?
+        this.order = params.order;
+        this.currentIndex = 0;
+    }
+
+    async load() { // request OK pas touche
+        const startIndex = this.currentIndex;
+        const endIndex = this.currentIndex + this.batchSize;
+
+        try {
+            let responsePictures = await fetch(`${url}?_start=${startIndex}&_end=${endIndex}&_limit=${batchSize}`);
+            let pictures = await responsePictures.json();
+            console.log(pictures[currentIndex].url);
+            // dans la bonne fonction, on affichera pictures[currentIndex] dans une loop for of sur pictures
+    
+        } catch (e) {
+            console.error(`An error occured ${e.message}`);
+        }
+    }
+
+    createArea() { // WIP
+        this.area = document.createElement('div');
+        this.area.classList.add("photo");
+
+        this.photo.style.width = this.photoWidth;
+        this.photo.style.height = this.photoHeight;
+
+        this.imgElement = document.createElement('img');
+        this.imgElement.src = this.imageData[this.currentIndex].src;
+        this.photo.appendChild(this.imgElement);
+    }
+
+    displayPictures() {
+        // how many
+        // order
+        // where => create zone
+        //for (const photo of photos) {
+            this.load()
+        // }
     }
 
     createBtn() {
         // si plus de 10 photos restantes on affiche le bouton
     }
 
-    getPictures() {
+}
 
-    }
+/* -------------------------------------------------------------------------------------------------- */
+/* ZONE DE TESTS */
 
-    load() {
+async function load() {
+    try {
+        const batchSize = 10;
+        const currentIndex = 0;
+        const startIndex = currentIndex;
+        const endIndex = currentIndex + batchSize;
+        const url = 'https://jsonplaceholder.typicode.com/photos';
 
-        // async function ou utiliser un fetch simple ?
-        // affiche l'icone de chargement puis await le resultat de la requete
-    }
+        // let responsePictures = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=20');
 
+        let responsePictures = await fetch(`${url}?_start=${startIndex}&_end=${endIndex}&_limit=${batchSize}`);
+        let pictures = await responsePictures.json();
+        console.log(`on récup l'url ${pictures[currentIndex].url}`);
+        // dans la bonne fonction, on affichera pictures[currentIndex] dans une loop for of sur pictures
 
-    displayPictures() {
-        // how many
-        // order
-        // where 
+    } catch (e) {
+        console.error(`An error occured ${e.message}`);
     }
 }
 
+load();
 
+/* -------------------------------------------------------------------------------------------------- */
 
 // Exemple d'appel minimum 
 // const pictures = new LoadPicture({
 //     url : 'https://jsonplaceholder.typicode.com/photos',
 // })
 
-// Tous les paramètres (un paramètre objet contenant des propriétée)
-const pictures = new LoadPicture(
-{
-    url : 'https://jsonplaceholder.typicode.com/photos',
-    area : 'main section.photo',
-    numberPhoto : 20,
-    order : 'ASC'
-}
-)
+// Tous les paramètres (un paramètre objet contenant des propriétés)
+// const pictures = new LoadPicture(
+//     {
+//         url: 'https://jsonplaceholder.typicode.com/photos',
+//         area: 'main section.photo',
+//         numberPhoto: 20,
+//         order: 'ASC'
+//     }
+// )
