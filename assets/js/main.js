c63c544892207c4948b129bf3576eb417bbe8d6a
Eva Doncieux <eva.doncieux@gmail.com>
2023-10-22 22:25:14 +0200

    fixed url issue, wip on the dom elements creation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,40 +10,31 @@ class LoadPicture {
         this.pictures = null;
         console.log('constructor');
 
-        this.createArea();
+        // this.createArea();
+        this.displayPictures();
     }
 
     createArea() {
         console.log('create area BEG');
+        const area = document.createElement('div');
+        area.classList.add("photo");
 
-        const img = new Image();
-        img.onload = () => {
-            const area = document.createElement('div');
-            area.classList.add("photo");
-            area.style.width = '100%';
-            area.style.height = '100%';
+        area.style.width = '100%';
+        area.style.height = '100%';
 
-            img.style.width = '100%';
-            img.style.height = '100%';
+        const img = document.createElement('img');
+        img.src = this.pictures[this.currentIndex];
+        area.appendChild(img);
+        console.log(img.src);
 
-            area.appendChild(img);
-            this.area.appendChild(area);
+        this.area.appendChild(area);
 
-            if (this.currentIndex >= 10) {
-                this.createBtn('next', area);
-            }
+        if (this.currentIndex >= 10) {
+            this.createBtn('next', area);
 
-            if (this.currentIndex >= 20) {
-                this.createBtn('previous', area);
-            }
-
-            this.displayPictures();
-        };
-
-        this.load().then(pictures => {
-            this.imageData = pictures.map(picture => picture.url);
-            img.src = this.imageData[this.currentIndex];
-        });
+        } else if (this.currentIndex >= 20) {
+            this.createBtn('previous', area);
+        }
         console.log('create area END');
     }
 
@@ -55,13 +46,13 @@ class LoadPicture {
         const endIndex = this.currentIndex + this.batchSize;
 
         try {
-            
+
             const responsePictures = await fetch(`${this.url}?_start=${startIndex}&_end=${endIndex}&_limit=${this.batchSize}`);
             const pictures = await responsePictures.json();
-            
+
             const url = pictures.map(picture => picture.url);
-            
-            console.log(url[this.currentIndex]);
+
+            // console.log(url[this.currentIndex]);
             console.log('load END');
             return url;
 
@@ -69,7 +60,7 @@ class LoadPicture {
             console.error(`An error occured ${e.message}`);
         }
     }
-    
+
     async displayPictures() {
         console.log('display pictures BEG');
 
@@ -103,47 +94,7 @@ class LoadPicture {
 
 /* -------------------------------------------------------------------------------------------------- */
 /* ZONE DE TESTS */
-// 
-// let photos = null;
-// let currentIndex = null;
-// 
-// async function load() {
-//     try {
-//         const batchSize = 10;
-//         currentIndex = 0;
-//         const startIndex = currentIndex;
-//         const endIndex = currentIndex + batchSize;
-//         const url = 'https://jsonplaceholder.typicode.com/photos';
-// 
-//         // let responsePictures = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=20');
-// 
-//         let responsePictures = await fetch(`${url}?_start=${startIndex}&_end=${endIndex}&_limit=${batchSize}`);
-//         pictures = await responsePictures.json();
-//         console.log(`on récup l'url ${photos[currentIndex].url}`);
-//         return photos;
-//         // dans la bonne fonction, on affichera pictures[currentIndex] dans une loop for of sur pictures
-// 
-//     } catch (e) {
-//         console.error(`An error occured ${e.message}`);
-//     }
-// }
-// 
-// load();
-// 
-// 
-// function createArea(photos) { // WIP
-//     const area = document.createElement('div');
-//     area.classList.add("photo");
-// 
-//     area.style.width = "100%";
-//     area.style.height = "100%";
-// 
-//     const img = document.createElement('img');
-//     img.src = photos[currentIndex].url;
-//     photo.appendChild();
-// }
-// 
-// createArea();
+
 
 /* -------------------------------------------------------------------------------------------------- */
 
@@ -162,4 +113,4 @@ const pictures = new LoadPicture(
     }
 )
 
-// console.log('hellu')
+// console.log('hellu');
